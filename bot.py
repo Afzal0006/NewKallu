474d974c00064895766bcb9e8d3a1adef90dea24
Afzal0006 <121087528+Afzal0006@users.noreply.github.com>
2025-12-04 09:30:49 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from pymongo import MongoClient
 # ==== CONFIG ====
 BOT_TOKEN = "8370711516:AAEtXRjArAKwTlcZfx4dzd5E_aiIvcs4yvU"
 MONGO_URI = "mongodb+srv://afzal99550:afzal99550@cluster0.aqmbh9q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-OWNER_IDS = [7270006608]  # <-- Apna Telegram ID yaha daalna hai
+OWNER_IDS = [8280018677]  
 
 # ==== MongoDB Setup ====
 mongo = MongoClient(MONGO_URI)
